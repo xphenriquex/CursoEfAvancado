c0c72c0e575008b6ae5a38afe71d6706ce6e6c17
Paulo Henrique <p.henriquerm@hotmail.com>
2021-12-18 19:13:51 -0300

    Atributo Index

--- Domain/Atributo.cs
+++ Domain/Atributo.cs
@@ -1,10 +1,13 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
 
 namespace CursoEfAvancado.Domain
 {
     [Table("TabelaAtributos")]
+    [Index(nameof(Descricao), nameof(Id), IsUnique = true)]
+
     public class Atributo
     {
         [Key]
