d8ffd6d3cb6292b430c1c52bf49a499dcc312ecd
Paulo Henrique <p.henriquerm@hotmail.com>
2021-12-21 14:10:42 -0300

    Funções de datas Função Like Função DataLength Função Property Função Collate

--- Data/ApplicationContext.cs
+++ Data/ApplicationContext.cs
@@ -1,32 +1,13 @@
 using System;
-using System.Collections.Generic;
-using System.Reflection;
-using CursoEfAvancado.Conversores;
 using CursoEfAvancado.Domain;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Microsoft.Extensions.Logging;
 
 namespace CursoEfAvancado.Data
 {
     public class ApplicationContext : DbContext
     {
-        //private readonly StreamWriter _writer = new StreamWriter("meu_log_do_ef_core.txt", append: true);
-        public DbSet<Departamento> Departamentos { get; set; }
-        public DbSet<Funcionario> Funcionarios { get; set; }
-        public DbSet<Estado> Estados { get; set; }
-        public DbSet<Conversor> Conversores { get; set; }
-        public DbSet<Cliente> Clientes { get; set; }
-        public DbSet<Cidade> Cidades { get; set; }
-        public DbSet<Ator> Atores { get; set; }
-        public DbSet<Filme> Filmes { get; set; }
-        public DbSet<Documento> Documentos { get; set; }
-        public DbSet<Pessoa> Pessoas { get; set; }
-        public DbSet<Instrutor> Instrutores { get; set; }
-        public DbSet<Aluno> Alunos { get; set; }
-        public DbSet<Dictionary<string, object>> Configuracoes => Set<Dictionary<string, object>>("Configuracoes");
-        public DbSet<Atributo> Atributos { get; set; }
-        public DbSet<Aeroporto> Aeroportos { get; set; }
+        public DbSet<Funcao> Funcoes {get;set;}
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
@@ -39,75 +20,13 @@ namespace CursoEfAvancado.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            /*modelBuilder.UseCollation("SQL_Latin1_General_CP1_CI_AI");
-            //RAFAEL -> rafael
-            //Jõao -> Joao
-
-            modelBuilder.Entity<Departamento>().Property(p=>p.Descricao).UseCollation("SQL_Latin1_General_CP1_CS_AS");
-
-              modelBuilder
-                .HasSequence<int>("MinhaSequencia", "sequencias")
-                .StartsAt(1)
-                .IncrementsBy(2)
-                .HasMin(1)
-                .HasMax(10)
-                .IsCyclic();
-
-            modelBuilder.Entity<Departamento>().Property(p=>p.Id).HasDefaultValueSql("NEXT VALUE FOR sequencias.MinhaSequencia");
-            */
-
-            /*modelBuilder
-                .Entity<Departamento>()
-                .HasIndex(p=> new { p.Descricao,s p.Ativo})
-                .HasDatabaseName("idx_meu_indice_composto")
-                .HasFilter("Descricao IS NOT NULL")
-                .HasFillFactor(80)
-                .IsUnique();*/
-
-            /*modelBuilder.Entity<Estado>().HasData(new[] 
-            {
-                new Estado { Id = 1, Nome = "Sao Paulo"},
-                new Estado { Id = 2, Nome = "Sergipe"}
-            });*/
-
-            /*modelBuilder.HasDefaultSchema("cadastros"); 
-            modelBuilder.Entity<Estado>().ToTable("Estados", "SegundoEsquema");*/
-
-            /*var conversao = new ValueConverter<Versao, string>(p => p.ToString(), p => (Versao)Enum.Parse(typeof(Versao), p));
-
-            var conversao1 = new EnumToStringConverter<Versao>();
-
-            modelBuilder.Entity<Conversor>()
-                .Property(p => p.Versao)
-                .HasConversion(conversao1);*/
-            
-            //.HasConversion(conversao);
-            //.HasConversion(p=>p.ToString(), p=> (Versao)Enum.Parse(typeof(Versao), p));
-            //.HasConversion<string>();
-
-            /*modelBuilder.Entity<Conversor>()
-                .Property(p => p.Status)
-                .HasConversion(new ConversorCustomizado());*/
-
-            //modelBuilder.Entity<Departamento>().Property<DateTime>("UltimaAtualizacao");
-
-            //modelBuilder.ApplyConfiguration(new ClienteConfiguration());
-            //modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
-            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationContext).Assembly);
-
-             modelBuilder.SharedTypeEntity<Dictionary<string, object>>("Configuracoes", b =>
-            {
-                b.Property<int>("Id");
-
-                b.Property<string>("Chave")
-                    .HasColumnType("VARCHAR(40)")
-                    .IsRequired();
-
-                b.Property<string>("Valor")
-                    .HasColumnType("VARCHAR(255)")
-                    .IsRequired();
-            });
-
+            modelBuilder
+                .Entity<Funcao>(conf=>
+                {
+                    conf.Property<string>("PropriedadeSombra")
+                        .HasColumnType("VARCHAR(100)")
+                        .HasDefaultValueSql("'Teste'");
+                });
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -12,341 +12,160 @@ namespace CursoEfAvancado
         static void Main(string[] args)
         {
             using var db = new ApplicationContext();
+
+            //FuncoesDeDatas();
+            //FuncaoLike();
+            //FuncaoDataLength();
+            //FuncaoProperty();
+            FuncaoCollate();            
             
-            //Collations();
-            //PropagarDados();
-            //Esquema();
-            //ConversorDeValor();
-            //ConversorCustomizado();
-            //PropriedadesDeSombra();
-            //TrabalhandoComPropriedadesDeSombra();
-            //TiposDePropriedades();
-            //Relacionamento1Para1();
-            //Relacionamento1ParaMuitos();
-            //RelacionamentoMuitosParaMuitos();
-            //CampoDeApoio();
-            //ExemploTPH();
-            Atributos();
         }
 
-    
-        static void ConversorCustomizado()
-        {
-            using var db = new ApplicationContext();
-            db.Database.EnsureDeleted();
-            db.Database.EnsureCreated();
-
-            db.Conversores.Add(
-                new Conversor
-                {
-                    Status = Status.Devolvido,
-                });
-
-            db.SaveChanges();
-
-            var conversorEmAnalise = db.Conversores.AsNoTracking().FirstOrDefault(p => p.Status == Status.Analise);
-
-            var conversorDevolvido = db.Conversores.AsNoTracking().FirstOrDefault(p => p.Status == Status.Devolvido);
-        }
-
-        static void ConversorDeValor() => Esquema();
-
-         static void Esquema()
-        {
-            using var db = new ApplicationContext();
-
-            var script = db.Database.GenerateCreateScript();
-
-            Console.WriteLine(script);
-        }
-
-        static void PropagarDados()
-        {
-            using var db = new ApplicationContext();
-            db.Database.EnsureDeleted();
-            db.Database.EnsureCreated();
-
-            var script = db.Database.GenerateCreateScript();
-            Console.WriteLine(script);
-        }
-        
-        static void Collations()
-        {
-            using var db = new ApplicationContext();
-            db.Database.EnsureDeleted();
-            db.Database.EnsureCreated();
-        }
-
-        static void PropriedadesDeSombra()
-        {
-            using var db = new ApplicationContext();
-            db.Database.EnsureDeleted();
-            db.Database.EnsureCreated();
-        }
-
-        static void TrabalhandoComPropriedadesDeSombra()
-        {
-            using var db = new ApplicationContext();
-            /*db.Database.EnsureDeleted();
-            db.Database.EnsureCreated();
-
-            var departamento = new Departamento
-            {
-                Descricao = "Departamento Propriedade de Sombra"
-            };
-
-            db.Departamentos.Add(departamento);
-
-            db.Entry(departamento).Property("UltimaAtualizacao").CurrentValue = DateTime.Now;
-
-            db.SaveChanges();*/
-            
-
-            var departamentos = db.Departamentos.Where(p => EF.Property<DateTime>(p, "UltimaAtualizacao") < DateTime.Now).ToArray();
-        }
-
-        static void TiposDePropriedades()
-        {
-            using var db = new ApplicationContext();
-            db.Database.EnsureDeleted();
-            db.Database.EnsureCreated();
-
-            var cliente = new Cliente
-            {
-                Nome = "Fulano de tal",
-                Telefone = "(79) 98888-9999",
-                Endereco = new Endereco { Bairro = "Centro", Cidade = "Sao Paulo" }
-            };
-
-            db.Clientes.Add(cliente);
-
-            db.SaveChanges();
-
-            var clientes = db.Clientes.AsNoTracking().ToList();
-
-            var options = new System.Text.Json.JsonSerializerOptions { WriteIndented = true };
-
-            clientes.ForEach(cli =>
-            {
-                var json = System.Text.Json.JsonSerializer.Serialize(cli, options);
-
-                Console.WriteLine(json);
-            });
-        }
-
-        static void Relacionamento1Para1()
-        {
-            using var db = new ApplicationContext();
-            db.Database.EnsureDeleted();
-            db.Database.EnsureCreated();
-
-            var estado = new Estado
-            {
-                Nome = "Sergipe",
-                Governador = new Governador { Nome = "Rafael Almeida" }
-            };
-
-            db.Estados.Add(estado);
-
-            db.SaveChanges();
-
-            var estados = db.Estados.AsNoTracking().ToList();
-
-            estados.ForEach(est =>
-            {
-                Console.WriteLine($"Estado: {est.Nome}, Governador: {est.Governador.Nome}");
-            });
-        }
-
-        static void Relacionamento1ParaMuitos()
+        static void FuncaoCollate()
         {
             using (var db = new ApplicationContext())
             {
-                db.Database.EnsureDeleted();
-                db.Database.EnsureCreated();
 
-                var estado = new Estado
-                {
-                    Nome = "Sergipe",
-                    Governador = new Governador { Nome = "Rafael Almeida" }
-                };
+                var consulta1 = db
+                    .Funcoes
+                    .FirstOrDefault(p=> EF.Functions.Collate(p.Descricao1, "SQL_Latin1_General_CP1_CS_AS") == "Tela");
 
-                estado.Cidades.Add(new Cidade { Nome = "Itabaiana" });
+                var consulta2 = db
+                    .Funcoes
+                    .FirstOrDefault(p=> EF.Functions.Collate(p.Descricao1, "SQL_Latin1_General_CP1_CI_AS") == "tela");
 
-                db.Estados.Add(estado);
-
-                db.SaveChanges();
-            }
-
-            using (var db = new ApplicationContext())
-            {
-                var estados = db.Estados.ToList();
-
-                estados[0].Cidades.Add(new Cidade { Nome = "Aracaju" });
-
-                db.SaveChanges();
-
-                foreach (var est in db.Estados.Include(p => p.Cidades).AsNoTracking())
-                {
-                    Console.WriteLine($"Estado: {est.Nome}, Governador: {est.Governador.Nome}");
-
-                    foreach (var cidade in est.Cidades)
-                    {
-                        Console.WriteLine($"\t Cidade: {cidade.Nome}");
-                    }
-                }
-            }
-        }
-
-        static void RelacionamentoMuitosParaMuitos()
-        {
-            using (var db = new ApplicationContext())
-            {
-                db.Database.EnsureDeleted();
-                db.Database.EnsureCreated();
-
-                var ator1 = new Ator { Nome = "Rafael" };
-                var ator2 = new Ator { Nome = "Pires" };
-                var ator3 = new Ator { Nome = "Bruno" };
-
-                var filme1 = new Filme { Descricao = "A volta dos que não foram" };
-                var filme2 = new Filme { Descricao = "De volta para o futuro" };
-                var filme3 = new Filme { Descricao = "Poeira em alto mar filme" };
-
-                ator1.Filmes.Add(filme1);
-                ator1.Filmes.Add(filme2);
-
-                ator2.Filmes.Add(filme1);
-
-                filme3.Atores.Add(ator1);
-                filme3.Atores.Add(ator2);
-                filme3.Atores.Add(ator3);
-
-                db.AddRange(ator1, ator2, filme3);
-
-                db.SaveChanges();
-
-                foreach (var ator in db.Atores.Include(e => e.Filmes))
-                {
-                    Console.WriteLine($"Ator: {ator.Nome}");
-
-                    foreach (var filme in ator.Filmes)
-                    {
-                        Console.WriteLine($"\tFilme: {filme.Descricao}");
-                    }
-                }
-            }
-        }
-
-        static void CampoDeApoio()
-        {
-            using (var db = new ApplicationContext())
-            {
-                db.Database.EnsureDeleted();
-                db.Database.EnsureCreated();
-
-                var documento = new Documento();
-                documento.SetCPF("12345678933");
-
-                db.Documentos.Add(documento);
-                db.SaveChanges();
-
-                foreach (var doc in db.Documentos.AsNoTracking())
-                {
-                    Console.WriteLine($"CPF -> {doc.GetCPF()}");
-                }
-            }
-        }
-
-        static void ExemploTPH()
-        {
-            using (var db = new ApplicationContext())
-            {
-                db.Database.EnsureDeleted();
-                db.Database.EnsureCreated();
-
-                var pessoa = new Pessoa { Nome = "Fulano de Tal" };
-
-                var instrutor = new Instrutor { Nome = "Rafael Almeida", Tecnologia = ".NET", Desde = DateTime.Now };
-
-                var aluno = new Aluno { Nome = "Maria Thysbe", Idade = 31, DataContrato = DateTime.Now.AddDays(-1) };
-
-                db.AddRange(pessoa, instrutor, aluno);
-                db.SaveChanges();
-
-                var pessoas = db.Pessoas.AsNoTracking().ToArray();
-                var instrutores = db.Instrutores.AsNoTracking().ToArray();
-                //var alunos = db.Alunos.AsNoTracking().ToArray();
-                var alunos = db.Pessoas.OfType<Aluno>().AsNoTracking().ToArray();
-
-                Console.WriteLine("Pessoas **************");
-                foreach (var p in pessoas)
-                {
-                    Console.WriteLine($"Id: {p.Id} -> {p.Nome}");
-                }
-
-                Console.WriteLine("Instrutores **************");
-                foreach (var p in instrutores)
-                {
-                    Console.WriteLine($"Id: {p.Id} -> {p.Nome}, Tecnologia: {p.Tecnologia}, Desde: {p.Desde}");
-                }
-
-                Console.WriteLine("Alunos **************");
-                foreach (var p in alunos)
-                {
-                    Console.WriteLine($"Id: {p.Id} -> {p.Nome}, Idade: {p.Idade}, Data do Contrato: {p.DataContrato}");
-                }
-            }
-        }
-
-        static void PacotesDePropriedades()
-        {
-            using (var db = new ApplicationContext())
-            {
-                db.Database.EnsureDeleted();
-                db.Database.EnsureCreated();
-
-                var configuracao = new Dictionary<string, object>
-                {
-                    ["Chave"] = "SenhaBancoDeDados",
-                    ["Valor"] = Guid.NewGuid().ToString()
-                };
-
-                db.Configuracoes.Add(configuracao);
-                db.SaveChanges();
-
-                var configuracoes = db
-                    .Configuracoes
-                    .AsNoTracking()
-                    .Where(p => p["Chave"] == "SenhaBancoDeDados")
-                    .ToArray();
-
-                foreach (var dic in configuracoes)
-                {
-                    Console.WriteLine($"Chave: {dic["Chave"]} - Valor: {dic["Valor"]}");
-                }
-            }
-        }
-
-        static void Atributos()
-        {
-            using (var db = new ApplicationContext())
-            {
-                db.Database.EnsureDeleted();
-                db.Database.EnsureCreated();
+                Console.WriteLine($"Consulta1: {consulta1?.Descricao1}");
                 
+                Console.WriteLine($"Consulta2: {consulta2?.Descricao1}");
+            }
+        }
+
+        static void FuncaoProperty()
+        {
+            ApagarCriarBancoDeDados();
+
+            using (var db = new ApplicationContext())
+            {
+                var resultado = db
+                    .Funcoes
+                    //.AsNoTracking()
+                    .FirstOrDefault(p=> EF.Property<string>(p, "PropriedadeSombra") == "Teste");
+
+                var propriedadeSombra = db
+                    .Entry(resultado)
+                    .Property<string>("PropriedadeSombra")
+                    .CurrentValue;
+
+                Console.WriteLine("Resultado:");     
+                Console.WriteLine(propriedadeSombra); 
+            }
+        } 
+
+        static void FuncaoDataLength()
+        {
+            using (var db = new ApplicationContext())
+            {
+                var resultado = db
+                    .Funcoes
+                    .AsNoTracking()
+                    .Select(p => new 
+                    {
+                        TotalBytesCampoData = EF.Functions.DataLength(p.Data1),
+                        TotalBytes1 = EF.Functions.DataLength(p.Descricao1),
+                        TotalBytes2 = EF.Functions.DataLength(p.Descricao2),
+                        Total1 = p.Descricao1.Length,
+                        Total2 = p.Descricao2.Length
+                    })
+                    .FirstOrDefault();
+
+                Console.WriteLine("Resultado:"); 
+
+                Console.WriteLine(resultado); 
+            }
+        }
+
+        static void FuncaoLike()
+        {
+            using (var db = new ApplicationContext())
+            {
                 var script = db.Database.GenerateCreateScript();
 
                 Console.WriteLine(script);
 
-                db.Atributos.Add(new Atributo
-                {
-                    Descricao = "Exemplo",
-                    Observacao = "Observacao"
-                });
+                var dados = db
+                    .Funcoes
+                    .AsNoTracking()
+                    //.Where(p=> EF.Functions.Like(p.Descricao1, "Bo%"))
+                    .Where(p=> EF.Functions.Like(p.Descricao1, "B[ao]%"))
+                    .Select(p => p.Descricao1)
+                    .ToArray();
 
-                db.SaveChanges();
+                Console.WriteLine("Resultado:");
+                foreach (var descricao in dados)
+                {
+                    Console.WriteLine(descricao);
+                }
             }
         }
+
+        static void FuncoesDeDatas()
+        {
+            ApagarCriarBancoDeDados();
+
+            using (var db = new ApplicationContext())
+            {
+                var script = db.Database.GenerateCreateScript();
+
+                Console.WriteLine(script);
+
+                var dados = db.Funcoes.AsNoTracking().Select(p =>
+                   new
+                   {
+                       Dias = EF.Functions.DateDiffDay(DateTime.Now, p.Data1),
+                       Meses = EF.Functions.DateDiffMonth(DateTime.Now, p.Data1),
+                       Data = EF.Functions.DateFromParts(2021, 1, 2),
+                       DataValida = EF.Functions.IsDate(p.Data2),
+                   });
+
+                foreach (var f in dados)
+                {
+                    Console.WriteLine(f);
+                }
+
+            }
+        }
+
+        static void ApagarCriarBancoDeDados()
+        {
+            using var db = new ApplicationContext();
+            db.Database.EnsureDeleted();
+            db.Database.EnsureCreated();
+
+            db.Funcoes.AddRange(
+            new Funcao
+            {
+                Data1 = DateTime.Now.AddDays(2),
+                Data2 = "2021-01-01",
+                Descricao1 = "Bala 1 ",
+                Descricao2 = "Bala 1 "
+            },
+            new Funcao
+            {
+                Data1 = DateTime.Now.AddDays(1),
+                Data2 = "XX21-01-01",
+                Descricao1 = "Bola 2",
+                Descricao2 = "Bola 2"
+            },
+            new Funcao
+            {
+                Data1 = DateTime.Now.AddDays(1),
+                Data2 = "XX21-01-01",
+                Descricao1 = "Tela",
+                Descricao2 = "Tela"
+            });
+
+            db.SaveChanges();
+        }
+
     }
 }
